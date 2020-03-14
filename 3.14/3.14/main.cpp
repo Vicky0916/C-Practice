14f4ecc1db3eda8d32a3abe15b6b12c80e9e2063
Vickey0916 <271982953@qq.com>
2020-03-14 22:50:37 +0800

    commit

---
+++ 3.14/3.14/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+#include <vector>
+using namespace std;
+//int main()
+//{
+//		string str;
+//		while (getline(cin, str))
+//		{
+//			for (int i = 0; i < str.size(); i++)
+//			{
+//				if (str[i] >= '0'&&str[i] <= '9')
+//					cout << str[i];
+//			}
+//			cout << endl;
+//		}
+//	system("pause");
+//	return 0;
+//}
+int main()
+{
+	system("pause");
+	return 0;
+}
